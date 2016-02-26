4d4fc26c4ba77ca959df1e17f222432d49dbc4c9
ahmedam55 <ahmed42d44@gmail.com>
2016-02-26 14:36:40 -0800

    add side bar context menu feature to (Add/Remove) Folder/File to AutoComplete or Clear all Files and Folders from AutoComplete more refactoring and handling edge cases and test on bootstrap-sass repo

--- main.py
+++ main.py
@@ -2,10 +2,45 @@ import sublime
 import sublime_plugin
 import re
 import os
+import fnmatch
 
 class SassSolutionCommand(sublime_plugin.EventListener):
     def on_post_save(self, view):
         Engine.runEngine(self,view)
+
+
+class AddToAutoCompleteCommand(sublime_plugin.WindowCommand):
+    def run(self,paths=[]):
+
+        for x in paths:
+            if os.path.isfile(x):
+                filesList=Engine.getFiles()
+                filesList.append(x+'\\')
+                Engine.setFiles(filesList)
+            else:                
+                foldersList=Engine.getFolders()
+                foldersList.append(x+'\\')
+                Engine.setFolders(foldersList)
+
+        Engine.saveSettings()
+
+
+class RemoveFromAutoCompleteCommand(sublime_plugin.WindowCommand):
+    def run(self,paths=[]):
+
+        for x in paths:
+            settingsList=Engine.getFiles() if os.path.isfile(x) else Engine.getFiles()
+            settingsList.remove(x) 
+            Engine.loadSettings().set()
+
+        Engine.saveSettings()
+
+
+class ClearAutoCompleteCommand(sublime_plugin.WindowCommand):
+    def run(self,paths=[]):
+        Engine.eraseFiles()
+        Engine.eraseFolders()
+        Engine.saveSettings()
 
 
 
@@ -47,73 +82,109 @@ class SassSolutionCommand(sublime_plugin.EventListener):
 
 class Engine:
 
+    def isSass(myview):
+        extension='.scss'
+
+        if(isinstance(myview,sublime.View)):
+            return myview.file_name().endswith(extension)
+        else:
+            return myview.endswith(extension)
+
+
+    def loadSettings():
+        return sublime.load_settings('sassSolution.sublime-settings')
+
+
+    def saveSettings():
+        sublime.save_settings('sassSolution.sublime-settings')
+
+
+    def getFolders():
+        return Engine.loadSettings().get('folders')
+
+
+    def getFiles():
+        return Engine.loadSettings().get('files')
+
+
+    def setFolders(newList):
+        Engine.loadSettings().set('folders',newList)
+
+
+    def setFiles(newList):
+        Engine.loadSettings().set('files',newList)
+
+
+    def eraseFiles():
+        Engine.loadSettings().erase('files');
+
+
+    def eraseFolders():
+        Engine.loadSettings().erase('folders');
+
+
+    def getFoldersFilesRecursively(folder):
+        matches=[]
+
+        for root, dirnames, filenames in os.walk(folder):
+            for filename in fnmatch.filter(filenames, '*.scss'):
+                matches.append(os.path.join(root, filename))
+
+        return matches
+
+
+    def getFilesAndFoldersText(folders,files):
+        code=''
+        print(folders)
+
+        for x in folders:
+            for file in Engine.getFoldersFilesRecursively(x):
+                code+=open(file,'r', encoding="utf8").read()
+
+        for x in files:
+            code+=open(x,'r' ,encoding="utf8").read()
+
+        return code
+
+
+    def writeJsonFile(content):
+        path=sublime.packages_path()+'\\User\\sbc-api-mysass.sublime-settings'
+        f=open(path,'w',encoding="utf8")
+
+        contents = "".join(content)
+        f.write(contents)
+        f.close()
+
+
+    def removeSpecialChars(text,replaceDollar=True):
+        return text.replace('"',"").replace('\\','\\\\').replace('$','\\\$' if replaceDollar else '$')
+
+
+    def removeDollarSlashes(text):
+        return text.replace('\\','')
+
+
     def runEngine(self,view):
+        if Engine.isSass(view):   
 
-        def isSass(myview):
-            extension='.scss'
+                allSass=Engine.getFilesAndFoldersText(Engine.getFolders(),Engine.getFiles())
 
-            if(isinstance(myview,sublime.View)):
-                return myview.file_name().endswith(extension)
-            else:
-                return myview.endswith(extension)
+                jsonText='{"scope": "source.scss - string, source.scss","completions":[';
 
+                for x in re.findall(r'\$(.*?):(.*?);', allSass):
+                    variableName=Engine.removeSpecialChars(x[0])
+                    variableValue=Engine.removeSpecialChars(x[1],False)
 
-        def getFolders():
-            return sublime.load_settings('sassSolution.sublime-settings').get('folders')
+                    jsonText+='["'+('$'+variableName+'\t'+variableValue)+'","'+('\\\$'+variableName)+'"],'
 
+                for x in re.findall('\@mixin (.*?)\((.*?)\)',allSass):
+                    mixinName=Engine.removeSpecialChars(x[0])
+                    mixinArguments=Engine.removeSpecialChars(x[1])
 
-        def getFiles():
-            return sublime.load_settings('sassSolution.sublime-settings').get('files')
+                    zeroSlashesMixinArguments=Engine.removeDollarSlashes(mixinArguments)
 
+                    jsonText+='["'+mixinName+'('+zeroSlashesMixinArguments+')'+'","@include '+mixinName+'('+mixinArguments+')'+'"],'
 
-        def getFilesAndFoldersText(folders,files):
-            code=''
+                jsonText+=']}'
 
-            for x in folders:
-                for file in os.listdir(x):
-                    if isSass(file):
-                        code+=open(x+file,'r', encoding="utf8").read()
-
-            for x in files:
-                code+=open(x,'r' ,encoding="utf8").read()
-
-            return code
-
-
-        def writeJsonFile(content):
-            path=sublime.packages_path()+'\\User\\sbc-api-mysass.sublime-settings'
-            f=open(path,'w',encoding="utf8")
-
-            contents = "".join(content)
-            f.write(contents)
-            f.close()
-
-        def removeSpecialChars(text):
-            return text.replace('"',"").replace('\\','\\\\').replace('$','\\\$')
-
-        def callEngine():
-            if isSass(view):   
-
-                    allSass=getFilesAndFoldersText(getFolders(),getFiles())
-
-                    jsonText='{"scope": "source.scss - string, source.scss","completions":[';
-
-                    for x in re.findall(r'\$(.*?):(.*?);', allSass):
-                        variableName=removeSpecialChars(x[0])
-                        variableValue=removeSpecialChars(x[1])
-
-                        jsonText+='["'+('$'+variableName+'\t'+variableValue)+'","'+('\\\$'+variableName)+'"],'
-
-                    for x in re.findall('\@mixin (.*?)\((.*?)\)',allSass):
-                        mixinName=removeSpecialChars(x[0])
-                        mixinArguments=removeSpecialChars(x[1])
-
-                        jsonText+='["'+mixinName+'('+mixinArguments+')'+'","'+mixinName+'('+mixinArguments+')'+'"],'
-
-                    jsonText+=']}'
-
-                    writeJsonFile(jsonText)
-
-
-        #main function                
-        callEngine()
+                Engine.writeJsonFile(jsonText)
